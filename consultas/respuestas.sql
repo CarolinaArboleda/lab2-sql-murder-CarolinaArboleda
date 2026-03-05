bfc95c731263b8a1a1da2214cb8148c8beb8317d
Carolina Arboleda <carolina.arboleda4@udea.edu.co>
2026-03-05 11:05:29 -0500

    Paso 2 - identificación de testigos

--- consultas/respuestas.sql
+++ consultas/respuestas.sql
@@ -6,3 +6,15 @@ WHERE date = 20180115
 AND city = 'SQL City';
 
 
+-- Paso 2: Testigos
+
+SELECT *
+FROM person
+WHERE address_street_name = 'Northwestern Dr'
+ORDER BY address_number DESC
+LIMIT 1;
+
+SELECT *
+FROM person
+WHERE name LIKE 'Annabel%'
+AND address_street_name = 'Franklin Ave';
